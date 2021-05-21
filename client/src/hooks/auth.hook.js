6f98d4d63afce70a2018d3c5e9040668b22a06f0
böris <boris.yarmoulnik@gmail.com>
2021-05-21 13:52:05 +0300

    feat: add logic of create and auth page

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -4,6 +4,7 @@ const storageName = 'userData'
 
 export const useAuth = () => {
   const [token, setToken] = useState(null)
+  const [ready, setready] = useState(false)
   const [userId, setUserId] = useState(null)
 
   const login = useCallback((jwtToken, id) => {
--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export const DetailPage = () => {
+  const [link, setLink] = useState(initialState)
+
   return (
     <div>
       <h1>Detail Page</h1>
